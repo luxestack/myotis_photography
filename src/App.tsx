2f6e045c6374da4a808b1314e91adcc706a98218
kalani <cyberkalani@gmail.com>
2026-03-05 12:08:05 -0600

    Updates App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "@/pages/Index";
 import BandPage from "@/pages/BandPage";
 import About from "@/pages/About";
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/myotis_photography">
+      <HashRouter basename="/myotis_photography">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/band/:slug" element={<BandPage />} />
@@ -24,7 +24,7 @@ const App = () => (
           <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
