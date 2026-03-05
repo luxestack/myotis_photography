47adb8841926aed21f7bf0f8806eb220fbb28811
kalani <cyberkalani@gmail.com>
2026-03-05 11:41:17 -0600

    Final Commit <3

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { Toaster } from "./components/ui/toaster";
-import { Toaster as Sonner } from "./components/ui/sonner";
-import { TooltipProvider } from "./components/ui/tooltip";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import BandPage from "./pages/BandPage";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import NotFound from "./pages/NotFound";
+import Index from "@/pages/Index";
+import BandPage from "@/pages/BandPage";
+import About from "@/pages/About";
+import Contact from "@/pages/Contact";
+import NotFound from "@/pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/myotis_photography">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/band/:slug" element={<BandPage />} />
